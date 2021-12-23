4c582fe76d8cb29d7113cd1572a90b5f3f521138
Osman Canboğa <osmancanboga@hotmail.com>
2021-12-23 22:23:03 +0300

    Countdown timer - EggTimerAPP

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-
+    let eggTimes = ["Soft": 300, "Medium": 500, "Hard": 700]
+    var egg = 60
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        let hardness = sender.currentTitle!
+        egg = eggTimes[hardness]!
+        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+    }
+    @objc func updateCounter() {
+        //example functionality
+        if egg > 0 {
+            print("\(egg) seconds to the end of the world")
+            egg -= 1
+        }
+        
+        
+        
+    }
+    
 }
